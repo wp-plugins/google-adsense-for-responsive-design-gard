90d60837d2afc1324396ce79eba33ce2dd852e79
The Plugin Factory <The Plugin Factory@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-12 17:11:35 +0000

    Updated options page to include better notice for ad setup mode. Also added coupon for GARD Pro.
    git-svn-id: https://plugins.svn.wordpress.org/google-adsense-for-responsive-design-gard/tags/2.0.5@803208 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ adsizes.php
@@ -0,0 +1,28 @@
+<?php
+$adsizes = array(
+    #	Google Adsense Display and Text Unit Sizes
+		'970x90'  => 'Large Leaderboard', 
+		'728x90'  => 'Leaderboard',
+		'468x60'  => 'Banner ',
+		'336x280' => 'Large Rectangle',
+		'300x250' => 'Medium Rectangle',
+		'320x50'  => 'Mobile Banner',
+		'250x250' => 'Square ',
+		'234x60'  => 'Half Banner',
+		'125x125' => 'Button',
+		'120x600' => 'Skyscraper',
+		'160x600' => 'Wide Skyscraper',
+		'180x150' => 'Small Rectangle',
+		'120x240' => 'Vertical Banner',
+		'200x200' => 'Small Square',
+		'200x200' => 'Small Square',
+
+	#	Google Adsense Link Unit Sizes
+		'120x90'  => 'Displays 3 links',
+		'160x90'  => 'Displays 3 links',
+		'180x90'  => 'Displays 3 links',
+		'200x90'  => 'Displays 3 links',
+		'468x15'  => 'Displays 4 links',
+		'728x15'  => 'Displays 4 links',
+
+);
